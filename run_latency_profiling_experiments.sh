cfbdfacda8ea78a910b1cd90bb698f6d876baf48
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-14 20:31:44 -0700

    Add tarball creation to latency profling runscript

--- run_latency_profiling_experiments.sh
+++ run_latency_profiling_experiments.sh
@@ -5,3 +5,5 @@ cp ../../../run_cxl.sh .
 chmod +x run_cxl.sh
 cd ../../../experiments/latency_profling
 python3 latency_profiling.py
+cd ../../
+tar -zcvf latency_profiling.tar.gz experiments/latency_profiling
