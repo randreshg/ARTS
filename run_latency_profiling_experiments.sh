94d33ac94043b161903a2163c80bc64af7b139b7
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-15 08:29:04 -0700

    Clean experiments subdir

--- run_latency_profiling_experiments.sh
+++ run_latency_profiling_experiments.sh
@@ -4,6 +4,7 @@ cd build/example/cpu
 cp ../../../run_cxl.sh .
 chmod +x run_cxl.sh
 cd ../../../experiments/latency_profling
+rm -r */
 python3 latency_profiling.py
 cd ../../
 tar -zcvf latency_profiling.tar.gz experiments/latency_profiling
