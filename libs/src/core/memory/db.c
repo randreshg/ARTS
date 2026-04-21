0ecf2f8518d32c6ecbbee3bc5af3ddbe560e9d07
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-21 11:43:50 -0700

    Fix ALIGN_UP missing arg in db.c

--- libs/src/core/memory/db.c
+++ libs/src/core/memory/db.c
@@ -1134,7 +1134,7 @@ void arts_wait_reacquire_dbs(void) {
 void arts_cxl_producer_flush(arts_guid_t guid) {
   struct arts_db_s *db = (struct arts_db_s *)arts_cxl_get_ptr(guid);
   /* First flush the header to read the actual size. */
-  arts_printf("Flushing header of size: %lu\n", ALIGN_UP(sizeof(struct arts_db_s));
+  arts_printf("Flushing header of size: %lu\n", ALIGN_UP(sizeof(struct arts_db_s), CACHELINE_SIZE);
   FLUSH_FENCE_CONSUMER(db, ALIGN_UP(sizeof(struct arts_db_s), CACHELINE_SIZE));
   arts_printf("Producer flushing db of size: %lu\n", ALIGN_UP(db->header.size, CACHELINE_SIZE));
   FLUSH_FENCE_PRODUCER(db, ALIGN_UP(db->header.size, CACHELINE_SIZE));
