c19c01dc1b82f764ef953dd2332b2e0d7da48423
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-28 10:46:14 -0700

    Fix broken check logs script

--- check_logs_random_access.py
+++ check_logs_random_access.py
@@ -1,26 +1,58 @@
-from pathlib import Path                                                                                                                 │(basic) (basic) [alas515@twosisters2 ARTS]$ bc -l
-                                                                                                                                         │bc 1.07.1
-completion_str = "Verified"                                                                                                             │This is free software with ABSOLUTELY NO WARRANTY.
-                                                                                                                                         │12800/(192*4)
-log_files = list(Path('build/examples/cpu/').glob('*.log'))                                                                              │16.66666666666666666666
-                                                                                                                                         │760
-failed_files = []                                                                                                                        │760 * 760
-counter = 0                                                                                                                              │577600
-for f in log_files:                                                                                                                      │760 * 10
-    found = False                                                                                                                        │7600
-    contents = open(f, "r").read().split("\n")                                                                                           │760 * 256
-    for c in contents:                                                                                                                   │194560
-        if (completion_str in c):                                                                                                        │760 * 128
-            counter += 1                                                                                                                 │97280
-            found = True                                                                                                                 │2621440/760
-    if (not found):                                                                                                                      │3449.26315789473684210526
-        failed_files.append(f)                                                                                                           │760*1280
-                                                                                                                                         │972800
-print(f"Total runs: {len(log_files)}")                                                                                                   │
-print(f"Successful runs: {counter}")                                                                                                     │
-                                                                                                                                         │
-if (len(log_files) == counter):                                                                                                          │
-    print("Runs successful!")                                                                                                            │
-else:                                                                                                                                    │
-    print("Runs failed!")                                                                                                                │
+from pathlib import Path
+
+completion_str = "Verified"
+
+log_files = list(Path('build/examples/cpu/').glob('*.log'))
+
+failed_files = []
+counter = 0
+for f in log_files:
+    found = False
+    contents = open(f, "r").read().split("\n")
+    for c in contents:
+        if (completion_str in c):
+            counter += 1
+            found = True
+    if (not found):
+        failed_files.append(f)
+
+print(f"Total runs: {len(log_files)}")
+print(f"Successful runs: {counter}")
+
+if (len(log_files) == counter):
+    print("Runs successful!")
+else:
+    print("Runs failed!")
+    print("Failed files:", failed_files)
+(basic) (basic) [alas515@twosisters2 cpu]$ cat ../../../check_logs.py 
+from pathlib import Path
+
+#completion_str = "Solution Validates"
+#completion_str = "Verified"
+#completion_str = "Fib 25: 75025"
+completion_str = "Final Origin Energy"
+#completion_str = "PASS"
+
+log_files = list(Path('build/examples/cpu/').glob('*.log'))
+#log_files = list(Path('build_nonCXL/examples/cpu/').glob('*.log'))
+
+failed_files = []
+counter = 0
+for f in log_files:
+    found = False
+    contents = open(f, "r").read().split("\n")
+    for c in contents:
+        if (completion_str in c):
+            counter += 1
+            found = True
+    if (not found):
+        failed_files.append(f)
+
+print(f"Total runs: {len(log_files)}")
+print(f"Successful runs: {counter}")
+
+if (len(log_files) == counter):
+    print("Runs successful!")
+else:
+    print("Runs failed!")
     print("Failed files:", failed_files)
--- run_random_access_tests.sh
+++ run_random_access_tests.sh
@@ -10,4 +10,5 @@ cp ../../../sample_configs/arts_crete_roundrobin.cfg arts.cfg
 for i in {1..30}; do
     ./run_cxl.sh ./RANDOM_ACCESS &> random_access_${i}.log
 done
+cd ../../../
 python3 check_logs_random_access.py
