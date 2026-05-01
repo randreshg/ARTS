abeddab8f67a7c86d4129e8c709f6e5b11a2002b
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-05-01 08:21:55 -0700

    Add fibDB tests on 4 nodes

--- check_logs_fibDB.py
+++ check_logs_fibDB.py
@@ -1,13 +1,20 @@
 from pathlib import Path
 
+completion_str = "Fib 22: 17711"
+
 log_files = list(Path('build/examples/cpu/').glob('*.log'))
 
+failed_files = []
 counter = 0
 for f in log_files:
+    found = False
     contents = open(f, "r").read().split("\n")
     for c in contents:
-        if ("Fib 25: 75025" in c):
+        if (completion_str in c):
             counter += 1
+            found = True
+    if (not found):
+        failed_files.append(f)
 
 print(f"Total runs: {len(log_files)}")
 print(f"Successful runs: {counter}")
@@ -16,3 +23,4 @@ if (len(log_files) == counter):
     print("Runs successful!")
 else:
     print("Runs failed!")
+    print("Failed files:", failed_files)
---
+++ run_fibDB_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "**********************************"
+echo "Running round-robin DB allocation mode"
+cd build/examples/cpu
+cp ../../../run_cxl.sh .
+chmod +x run_cxl.sh
+cp ../../../sample_configs/arts_crete_roundrobin.cfg arts.cfg
+for i in {1..100}; do
+    ./run_cxl.sh ./fibDB 22 &> fibDB_${i}.log
+done
+cd ../../../
+python3 check_logs_random_access.py
