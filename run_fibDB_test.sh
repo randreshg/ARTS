cdec29282e67be1111a364ef1cbeb243db3a43d5
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-05-01 11:03:55 -0700

    Update fibDB runscript

--- run_fibDB_test.sh
+++ run_fibDB_test.sh
@@ -6,7 +6,7 @@ cd build/examples/cpu
 cp ../../../run_cxl.sh .
 chmod +x run_cxl.sh
 cp ../../../sample_configs/arts_crete_roundrobin.cfg arts.cfg
-for i in {1..100}; do
+for i in {1..50}; do
     ./run_cxl.sh ./fibDB 22 &> fibDB_${i}.log
 done
 cd ../../../
